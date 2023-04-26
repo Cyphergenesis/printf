03a6e045cf7d5d868e3ccecd00ebabd4c9936892
Cyphergenesis <josquilani200>
2023-04-26 05:18:30 +0100

    size

---
+++ size.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* get_size - Calculates the size to cast the argument
+* @format: Formatted string in which to print the arguments
+* @i: List of arguments to be printed.
+*
+* Return: Precision.
+*/
+int get_size(const char *format, int *i)
+{
+	int cur = *i + 1;
+	int size = 0;
+
+	if (format[cur] == 'l')
+	size = _LONG_;
+	else if (format[cur] == 'h')
+	size = _SHORT_;
+	if (size == 0)
+	*i = cur - 1;
+	else
+	*i = cur;
+
+	return (size);
+}
